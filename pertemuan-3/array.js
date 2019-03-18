38d40439b7d2175494dd0c25ae28a8958a05bb6a
Padlian Chairi <padlian25@gmail.com>
2019-03-18 19:48:30 +0700

    pertemuan 3 2

---
+++ pertemuan-3/array.js
@@ -0,0 +1,19 @@
+// let deret = ['padlian', 'chairi', 'Tamvan', 'rupawan'];
+
+// console.log(deret[0]);
+// console.log(deret[1]);
+// console.log(deret[2]);
+
+// let deret = [
+// 		['a','b','c','d'],
+// 		['e','f','g','h', 'ayeey'],
+// 		['i','j','k','l']
+// 	];
+
+// console.log(deret[1][4]);
+
+let deret = ['padlian', 'chairi'];
+deret[2]='tamvan';
+deret[3]='rupawan';
+
+console.log(deret[3]);
