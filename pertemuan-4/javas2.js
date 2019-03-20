5dda91aa45a4159fbf97e4bd14f2f327830fef10
Padlian Chairi <padlian25@gmail.com>
2019-03-20 19:39:23 +0700

    class

--- pertemuan-4/javas2.js
+++ pertemuan-4/javas2.js
@@ -1,3 +1,21 @@
-$("#btn1").click(function(){
-	$("#kotak").css("background", "RED")
+// $("#btn1").click(function(){
+// 	$("#kotak").css("background", "RED")
+// });
+
+// $("#btn2").click(function(){
+// 	$("#kotak").css("background", "GREEN")
+// });
+
+// $("#btn3").click(function(){
+// 	$("#kotak").css("background", "BLUE")
+// });
+
+// $("#btn4").click(function(){
+// 	$("#kotak").css("background", "none")
+// });
+
+
+$(".tombol").click(function(){
+	var warna = $(this).html();
+	$("#kotak").css("background", warna);
 });
