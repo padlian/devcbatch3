564133629cca04805f79445d527d5207679dc23c
Padlian Chairi <padlian25@gmail.com>
2019-03-20 19:25:32 +0700

    session 4

---
+++ pertemuan-4/javas.js
@@ -0,0 +1,7 @@
+$("h1").css("color", "red");
+
+$("h2").html("si tamvan");
+
+$("h3").html($("h1").html());
+
+$("#target").css("color", "green")
---
+++ pertemuan-4/javas2.js
@@ -0,0 +1,3 @@
+$("#btn1").click(function(){
+	$("#kotak").css("background", "RED")
+});
